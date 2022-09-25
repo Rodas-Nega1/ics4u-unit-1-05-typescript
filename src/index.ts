1a816ca4c2b0c7d82335c94bc2f4e47f5766770d
Rodas-Nega1 <Rodas-Nega1@users.noreply.github.com>
2022-09-25 13:35:40 +0000

    Prettified Code!

--- src/index.ts
+++ src/index.ts
@@ -7,36 +7,36 @@
  * Since:   2022-09-24
  */
 
-import promptSync from 'prompt-sync'
+import promptSync from "prompt-sync";
 
 /**
  * @param {number} userHeight parameter one as a number
  * @param {number} userWidth parameter two as a number
  * @returns {number} return userLength value as a number
  */
-function BoardFoot (userHeight: number, userWidth: number): number {
+function BoardFoot(userHeight: number, userWidth: number): number {
   // process
-  const volume = 144
-  const userLength = volume / (userWidth * userHeight)
+  const volume = 144;
+  const userLength = volume / (userWidth * userHeight);
 
-  return userLength
+  return userLength;
 }
 
-const prompt = promptSync()
+const prompt = promptSync();
 
 try {
   // input & output
-  const numberInput = prompt('Enter a width: ')
-  const userWidth = parseFloat(numberInput)
-  const numberInputTwo = prompt('Enter a height: ')
-  const userHeight = parseFloat(numberInputTwo)
+  const numberInput = prompt("Enter a width: ");
+  const userWidth = parseFloat(numberInput);
+  const numberInputTwo = prompt("Enter a height: ");
+  const userHeight = parseFloat(numberInputTwo);
 
   if (isNaN(userWidth) || isNaN(userHeight)) {
-    throw new Error('\nThat is an invalid number.')
+    throw new Error("\nThat is an invalid number.");
   }
 
   if (userWidth <= 0 || userHeight <= 0) {
-    throw new Error('\nThat is an invalid number.')
+    throw new Error("\nThat is an invalid number.");
   }
 
   console.log(
@@ -44,8 +44,8 @@ try {
       userHeight,
       userWidth
     )} inches for the board foot to be 144 inches³.`
-  )
-  console.log('\nDone.')
+  );
+  console.log("\nDone.");
 } catch (e) {
-  console.log('\nThat is an invalid input.')
+  console.log("\nThat is an invalid input.");
 }
